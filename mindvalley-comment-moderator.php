45fe2cc30fb46fe9e96427e139b17be0620e4cb9
mindvalley <mindvalley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-27 10:38:29 +0000

    initial
    git-svn-id: https://plugins.svn.wordpress.org/mindvalley-comment-moderator/trunk@401639 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mindvalley-comment-moderator.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: Mindvalley Comments Moderator
+Plugin URI: http://mindvalley.com
+Description: Create a custom role that enables only Comment Moderation actions and pages.
+Author: Mindvalley
+Version: 1.0
+*/
+
+class MV_Comment_Moderator {
+	function __construct(){
+		$this->add_role();
+		$this->add_cap();
+		add_action( 'admin_bar_menu', array(&$this, 'wp_admin_bar_comments_menu'), 50 );
+		add_action( 'admin_menu', array(&$this, 'admin_menu'));
+		add_action( 'admin_init', array(&$this, 'role_edit'));
+	}
+	
+	
+	function admin_menu(){
+		if(!current_user_can( 'mv_moderate_comments' )) {
+			return;
+		}
+		
+		$awaiting_mod = wp_count_comments();
+		$awaiting_mod = $awaiting_mod->moderated;
+		$awaiting_mod = $awaiting_mod ? "<span id='ab-awaiting-mod' class='pending-count'>" . number_format_i18n( $awaiting_mod ) . "</span>" : '';
+	
+		add_submenu_page( 'edit-comments.php', sprintf( __('Comments %s'), $awaiting_mod ), 'Comments' , 'mv_moderate_comments','edit-comments.php');
+	
+	}
+	
+	function role_edit(){
+		// Only do this when capabilities = mv_moderate_comments
+		if(current_user_can( 'mv_moderate_comments' )) {
+			global $pagenow;
+			
+			// Only allow access on edit-comments.php
+			if(	$pagenow == 'edit-comments.php' || 
+				$pagenow == 'comment.php' || 
+			   ($pagenow == 'admin-ajax.php' && (	$_POST['action'] == 'edit-comment' || 
+													$_POST['action'] == 'delete-comment' || 
+													$_POST['action'] == 'replyto-comment')) ){
+				$role = get_role('mv_comment_moderator');
+				$role->add_cap('edit_posts');
+				$role->add_cap('edit_published_posts');
+				$role->add_cap('edit_others_posts');
+			}else{
+				$role = get_role('mv_comment_moderator');
+				$role->remove_cap('edit_posts');
+				$role->remove_cap('edit_published_posts');
+				$role->remove_cap('edit_others_posts');
+			}
+			
+			// Flushing
+			global $current_user;
+			$current_user = '';
+			get_currentuserinfo();
+		}
+	}
+	
+	function wp_admin_bar_comments_menu(){
+		if(!current_user_can( 'mv_moderate_comments' )) {
+			return;
+		}
+		
+		global $wp_admin_bar;
+		
+		$awaiting_mod = wp_count_comments();
+		$awaiting_mod = $awaiting_mod->moderated;
+
+		$awaiting_mod = $awaiting_mod ? "<span id='ab-awaiting-mod' class='pending-count'>" . number_format_i18n( $awaiting_mod ) . "</span>" : '';
+		$wp_admin_bar->add_menu( array( 'id' => 'comments', 'title' => sprintf( __('Comments %s'), $awaiting_mod ), 'href' => admin_url('edit-comments.php') ) );
+	}
+	
+	function add_role(){
+		remove_role('mv_comment_moderator');
+		add_role('mv_comment_moderator', 'Comments Moderator');
+	}
+
+	function add_cap(){
+		$role = get_role('mv_comment_moderator');
+		$role->add_cap('mv_moderate_comments');
+		
+		// Base Capabilities for comments moderation
+		$role->add_cap('read');
+		$role->add_cap('moderate_comments');
+		$role->add_cap('edit_comment');
+	}
+}
+new MV_Comment_Moderator();
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Mindvalley Comments Moderator ===
+Contributors: mindvalley
+Donate link: http://www.mindvalley.com/opensource
+Tags: comment, moderator, moderation, user role
+Requires at least: 3.0.0
+Tested up to: 3.1.3
+Stable tag: 1.0
+
+Create a custom role that enables only Comment Moderation actions and pages.
+
+
+== Description ==
+
+It is a pain when WordPress needs the 'edit_posts' role when you JUST need the Comments SubPanel. Which makes it hard if you want to assign somebody as a simple Comments Moderator.
+
+This plugin will create a special user role called "Comments Moderator" that you can use to create any user account with. They will have access to the Comments SubPanel and all its functions, but nothing else :)
+
+
+
+== Installation ==
+
+1. Upload plugin folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Enjoy!
+
+
+
+
+== Screenshots ==
+
+1. New User Role - Comments Moderator
+2. Comment Moderator's Dashboard
+3. Comments SubPanel
+4. Comment Moderator's Admin Bar
+
+
+
+== Changelog ==
+
+= 1.0 =
+Initial Release.
